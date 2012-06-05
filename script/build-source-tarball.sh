e7f2a6858c4c7c18f302b8b80e02b52fabd9b374
Ole Wolf <wolf@blazingangles.com>
2012-06-05 22:49:17 +0200

    Build for both Debian and automake.

--- script/build-source-tarball.sh
+++ script/build-source-tarball.sh
@@ -15,9 +15,7 @@ fi
 
 # Create a tarball name based on the package directory.
 DIR=`basename $PWD`
-TARBALLNAME=adf
 PACKAGE=`echo ${DIR} | sed -n "s/-\([0-9]\+\.[0-9]\+\)//p"`
 VERSION=`echo ${DIR} | sed -n "s/${PACKAGE}-\([0-9]\+\.[0-9]\+\)/\1/p"`
-
 cd ..
-tar -cjf ${PACKAGE}_${VERSION}.orig.tar.xz --auto-compress --exclude=${DIR}/debian/* --exclude=${DIR}/.git/* --exclude=${DIR}/.gitignore ${DIR}
+tar -cjhf ${PACKAGE}_${VERSION}.orig.tar.xz --auto-compress --exclude=${DIR}/debian/* --exclude=${DIR}/.git/* --exclude=${DIR}/.gitignore ${DIR}
