19e84c6a5852cd2c58ddc8c835f56192c942ea51
Ole Wolf <wolf@blazingangles.com>
2012-06-10 15:08:21 +0200

    Singly linked list removed.

--- test/test-cache.c
+++ test/test-cache.c
@@ -1,5 +1,5 @@
 /**
- * \file test_list.c
+ * \file test_cache.c
  * \brief .
  *
  * Copyright (C) 2012 Ole Wolf <wolf@blazingangles.com>
@@ -23,77 +23,12 @@
 #include <config.h>
 #include <stdio.h>
 #include "testfunctions.h"
-#include "singlelist.h"
-
-
-
-void test_FillList( const char * );
-void test_DeleteNode( const char * );
-
-
-
-struct SingleList list =
-{
-    .first = NULL
-};
-
-
-int data1 = 1;
-int data2 = 2;
-int data3 = 3;
-int data4 = 4;
-
-struct Node node1 = { NULL, &data1 };
-struct Node node2 = { NULL, &data2 };
-struct Node node3 = { NULL, &data3 };
-struct Node node4 = { NULL, &data4 };
 
 
 
 const struct dispatchTable dispatchTable[ ] =
 {
-    { "FillList", &test_FillList },
-    { "DeleteNode", &test_DeleteNode },
     { NULL, NULL }
 };
 
 
-static void PrintList( struct SingleList *list )
-{
-    int         *data;
-    struct Node *node = list->first;
-    while( node != NULL )
-    {
-        data = node->data;
-        printf( "%d ", *data );
-        node = node->next;
-    }
-    printf( "\n" );
-}
-
-
-
-void test_FillList( const char *parms )
-{
-    /* Add item. */
-    InsertNodeAtHead( &list, &node1 );
-    AddNodeAfter( &node1, &node2 );
-    InsertNodeAtHead( &list, &node3 );
-    AddNodeAfter( &node1, &node4 );
-    PrintList( &list );
-}
-
-
-void test_DeleteNode( const char *parms )
-{
-    test_FillList( NULL );
-    /* Delete first item. */
-    DeleteNode( &list, &node3 );
-    PrintList( &list );
-    /* Delete second item. */
-    DeleteNode( &list, &node4 );
-    PrintList( &list );
-    /* Delete last item. */
-    DeleteNode( &list, &node2 );
-    PrintList( &list );
-}
